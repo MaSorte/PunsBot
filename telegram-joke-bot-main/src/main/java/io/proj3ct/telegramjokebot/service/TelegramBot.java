7118b8244bef43b3f29588be34383ef2274c1a49
MaSorte <161929933+MaSorte@users.noreply.github.com>
2024-05-04 17:54:23 +0300

    Update TelegramBot.java

--- telegram-joke-bot-main/src/main/java/io/proj3ct/telegramjokebot/service/TelegramBot.java
+++ telegram-joke-bot-main/src/main/java/io/proj3ct/telegramjokebot/service/TelegramBot.java
@@ -4,60 +4,36 @@ import io.proj3ct.telegramjokebot.config.BotConfig;
 import io.proj3ct.telegramjokebot.model.PunRepository;
 import io.proj3ct.telegramjokebot.model.PuriPuns;
 import com.vdurmont.emoji.EmojiParser;
-import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
 import java.sql.*;
-import java.util.*;
 import java.sql.Date;
+import java.util.*;
 
-@RestController
-@RequiredArgsConstructor
-@Service
 @Component
+@Service
+@RestController
 @Slf4j
-public class TelegramBot extends TelegramLongPollingBot {
+@RequestMapping("/api")
+public abstract class TelegramBot extends TelegramLongPollingBot {
 
     final BotConfig config;
-
-    @Override
-    public String getBotUsername() {
-        return config.getBotUserName();
-    }
-
-    @Override
-    public String getBotToken() {
-        return config.getToken();
-    }
-
-    static final String HELP_TEXT = """
-            Эта Система не знает, как может помочь Пользователю.
-            У этой Системы нет для тебя совета.\s
-            (ᓀ ᓀ)
-            Хочешь шутку, Пользователь? /pun""";
-    static final String INFO_TEXT = """
-            Сейчас доступны лишь эти команды, Пользователь:
-            /pun - выведет тебе шутку
-            /random_pun - выведет рандомную шутку
-            /all_pun - выведет все шутки
-            /add - напиши за командой шутку
-            /del - напиши за командой id нужной шутки
-            /pun_id - напиши за командой id нужной шутки
-            /help - всё довольно очевидно
-            """;
 
     @Autowired
     private PunRepository punRepository;
@@ -70,6 +46,30 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Value("${spring.datasource.password}")
     private String dbPassword;
+
+    static final String HELP_TEXT = """
+            Эта Система не знает, как может помочь Пользователю.
+            У этой Системы нет для тебя совета.\s
+            (ᓀ ᓀ)
+            Хочешь шутку, Пользователь? /pun""";
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "начало работы"));
+        listOfCommands.add(new BotCommand("/pun", "выведет шутку"));
+        listOfCommands.add(new BotCommand("/random_pun", "выведет рандомную шутку"));
+        listOfCommands.add(new BotCommand("/all_pun", "выведет все шутки"));
+        listOfCommands.add(new BotCommand("/add", "шутку писать за командой"));
+        listOfCommands.add(new BotCommand("/pun_id", "id писать за командой"));
+        listOfCommands.add(new BotCommand("/help", "ничего такого"));
+        listOfCommands.add(new BotCommand("/del", "удалить шутку"));
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error(Arrays.toString(e.getStackTrace()));
+        }
+    }
 
     public int getTotalPunsCount() {
         int totalPunsCount = 0;
@@ -84,15 +84,15 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
         return totalPunsCount;
     }
+
     private int currentPunIndex = 0;
 
     @SneakyThrows
-    @Override
-    public void onUpdateReceived(Update update) {
+    @PostMapping("/update")
+    public void onUpdateReceived(@RequestBody Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
-
             if ("/start".equals(messageText)) {
                 showStart(chatId);
             } else if ("/help".equals(messageText)) {
@@ -101,20 +101,17 @@ public class TelegramBot extends TelegramLongPollingBot {
                 punn(chatId);
             } else if ("/random_pun".equals(messageText)) {
                 rand_pun(chatId);
+            } else if (update.getMessage().getText().startsWith("/add ")) {
+                addpun(chatId, update);
             } else if ("/all_pun".equals(messageText)) {
                 sendAllPuns(chatId);
-            }else if (update.getMessage().getText().startsWith("/add ")) {
-                addpun(chatId, update);
             } else if ("/add".equals(messageText)) {
                 sendMessage(EmojiParser.parseToUnicode("Вы не ввели шутку."), chatId);
             } else if (update.getMessage().getText().startsWith("/del ")) {
                 del(chatId, update);
             } else if (update.getMessage().getText().startsWith("/pun_id ")) {
                 pun_id(chatId, update);
-            } else if ("/info".equals(messageText)) {
-                sendMessage(INFO_TEXT, chatId);
-            }
-            else {
+            } else {
                 commandNotFound(chatId);
             }
         }
@@ -123,18 +120,20 @@ public class TelegramBot extends TelegramLongPollingBot {
     public void punn(long chatId) {
         var pp = punRepository.findById((long) (currentPunIndex % getTotalPunsCount() + 1));
         if (pp.isPresent()) {
-            sendMessage("Шутка №" + (currentPunIndex+1) + ":\n" + pp.get().getBody()
+            sendMessage("Шутка №" + (currentPunIndex + 1) + ":\n" + pp.get().getBody()
                     + "\nДата создания: " + pp.get().getDateCreate(), chatId);
             currentPunIndex = (currentPunIndex + 1) % getTotalPunsCount();
         }
     }
+
     public void rand_pun(long chatId) {
         var r = new Random();
         var randomId = r.nextInt(getTotalPunsCount()) + 1;
         var pp = punRepository.findById((long) randomId);
-        pp.ifPresent(randomPun -> sendMessage("Шутка #" + (randomId+1) + ":\n" + randomPun.getBody()
+        pp.ifPresent(randomPun -> sendMessage("Шутка #" + (randomId + 1) + ":\n" + randomPun.getBody()
                 + "\nДата создания: " + pp.get().getDateCreate(), chatId));
     }
+
     public void sendAllPuns(long chatId) {
         for (int i = 1; i <= getTotalPunsCount(); i++) {
             var pun = punRepository.findById((long) i);
@@ -143,7 +142,8 @@ public class TelegramBot extends TelegramLongPollingBot {
                     + "\nДата создания: " + puriPuns.getDateCreate(), chatId));
         }
     }
-    public void pun_id(long chatId, Update update){
+
+    public void pun_id(long chatId, Update update) {
         String text = update.getMessage().getText();
         long punId = Integer.parseInt(text.substring(text.indexOf(" ") + 1).trim());
         var pp = punRepository.findById(punId);
@@ -152,6 +152,7 @@ public class TelegramBot extends TelegramLongPollingBot {
                     + "\nДата создания: " + pp.get().getDateCreate(), chatId);
         }
     }
+
     public void addpun(long chatId, @NotNull Update update) {
         var text = update.getMessage().getText().substring(update.getMessage().getText().indexOf(" ") + 1).trim();
         int Id = getTotalPunsCount() + 1;
@@ -162,7 +163,8 @@ public class TelegramBot extends TelegramLongPollingBot {
         punRepository.save(newpun);
         sendMessage("Шутка успешно добавлена", chatId);
     }
-    public void del(long chatId, Update update){
+
+    public void del(long chatId, Update update) {
         String text = update.getMessage().getText();
         int punId = Integer.parseInt(text.substring(text.indexOf(" ") + 1).trim());
         punRepository.deleteById(Long.valueOf(punId));
@@ -177,20 +179,22 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
     private void showStart(long chatId) {
-        String answer = EmojiParser.parseToUnicode("Привет, дорогой Пользователь.\nКоличество доступных шуток: " + getTotalPunsCount()
-        + "\nНужна информация? Нажми /info");
+        String answer = EmojiParser.parseToUnicode("Привет, дорогой Пользователь.\nКоличество доступных шуток: " + getTotalPunsCount());
         sendMessage(answer, chatId);
     }
+
     private void commandNotFound(long chatId) {
         String answer = EmojiParser.parseToUnicode("Команда не распознана.");
         sendMessage(answer, chatId);
     }
+
     private void sendMessage(String textToSend, long chatId) {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setText(textToSend);
         send(message);
     }
+
     private void send(SendMessage msg) {
         try {
             execute(msg);
